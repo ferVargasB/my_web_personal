b1ae46991a8522f037606bf8995fb01d886a143b
ferVargasB <f.vargas.bravo@icloud.com>
2019-11-14 15:27:07 -0600

    añadieron descripcion a los campos para español

--- webpersonal/portafolio/models.py
+++ webpersonal/portafolio/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Project(models.Model):
+    title = models.CharField(max_length=200,verbose_name="Título")
+    description = models.TextField(verbose_name="Descripción")
+    image = models.ImageField(verbose_name="Imagen del Proyecto")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Creado")
+    updated = models.DateTimeField(auto_now=True, verbose_name="Ultima Actualización")
+
+    class Meta:
+        ordering = ['-created']
+        verbose_name = "Proyecto"
+    
+    def __str__(self):
+        return self.title     
+
+
+
