2895743ef2b1b69aabd10b87243d74cc752b3095
ferVargasB <f.vargas.bravo@icloud.com>
2019-11-14 15:01:41 -0600

    Fusionó el front end y se adaptó a las templates
    Se creó la app portafolio por separado para manejar el modelo

--- webpersonal/webpersonal/urls.py
+++ webpersonal/webpersonal/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.home, name="home"),
+    path('about/', views.about, name="about"),
+    path('contact/', views.contact, name="contact"),
+    path('portafolio/', views.portafolio, name="portafolio"),
+    path('admin/', admin.site.urls, name="home"),
 ]
